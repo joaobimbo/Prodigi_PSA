4d1af13a4297e239bd0004507b54042812cae798
Joao Bimbo <joaobimbo@users.noreply.github.com>
2025-09-21 21:26:59 +0100

    warnings

--- webots_psa/controllers/psa_pid/psa_pid.py
+++ webots_psa/controllers/psa_pid/psa_pid.py
@@ -24,7 +24,6 @@ def plot(ps,t=100):
     
 
 robot=Robot()
-d=Display('POSITION_ERROR_GRAPH')
 timestep = int(robot.getBasicTimeStep())
 
 devs={}
@@ -51,18 +50,11 @@ def get_pos():
 e_prev=0
 i_e=0
 print(devs.keys())
+
+set_point=1.0
+
 while robot.step(timestep) != -1:
     p = get_pos() #obter o valor do sensor esquerdo
     ps.append(p)
-    #if len(ps)>100:
-    #    ps.pop(0)
     plot(ps)
-        
-    e=1-p
-    de=(e-e_prev)
-    i_e=i_e+e
-    set_torque(e*50.0+0*de*50+5*i_e*0)
-
-    e_prev=e
-
    
